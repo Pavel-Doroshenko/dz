8ab2d12650cf0d17bb7ac3d0dc5d79a934c16da0
Павел Дорошенко <grelkin34@gmail.com>
2025-03-19 16:00:08 +0000

    Продолжение добавления предпросмотра

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 const addBtnElement = document.getElementById("addBtn");
 const removeBtnElement = document.getElementById("removeBtn");
-const sliderElement = document.getElementById("slider");
-const newImageElement = document.getElementById("newImg");
+//const sliderElement = document.getElementById("slider");
+//const newImageElement = document.getElementById("newImg");
+
+function init() {
+  document.querySelector(".newImg").onclick = refresh;
+  document.querySelector(".addBtn").onclick = addImg;
+  document.querySelector(".removeBtn").onclick = removeImg;
+  document.querySelector(".container").addEventListener("click", zoomIn);
+  document.querySelectorAll("img").forEach((image) => {
+    image.onload = (Event) => Event.target.classList.remove("loading");
+  });
+  console.log("init");
+}
 
 function refresh() {
   document.querySelectorAll("img").forEach((image) => {
     image.src =
       "https://via.assets.so/album.png?id=1&q=95&w=360&h=360&fit=fill";
+    image.classList.add("loading");
   });
-}
-
-document.getElementById("newImg").onclick = refresh;
-
-addBtnElement.addEventListener("click", function () {
-  const newImage = document.createElement("img");
-  newImage.src =
-    "https://via.assets.so/game.png?id=1&q=95&w=360&h=360&fit=fill";
-  newImage.alt = "photo 2";
-
-  sliderElement.appendChild(newImage);
-
-  disabledBtn();
-});
-
-removeBtnElement.addEventListener("click", function () {
-  sliderElement.removeChild(sliderElement.lastChild);
-  disabledBtn();
-});
-
-function disabledBtn() {
-  let imageElement = document.querySelectorAll("img");
-  if (imageElement.length < 2) {
-    removeBtnElement.disabled = true;
-  } else {
-    removeBtnElement.disabled = false;
-  }
+  console.log("refresh");
 }
 
 function zoomIn(event) {
@@ -45,13 +31,35 @@ function zoomIn(event) {
   if (event.target.classList.contains("fullScreen")) {
     event.target.classList.toggle("fullScreen");
   }
+  console.log("zoomIn");
 }
 
-function init() {
-  document.getElementById("newImg").onclick = refresh;
-  document.querySelectorAll("img").forEach((image) => {
-    image.onclick = zoomIn;
-  });
+function addImg() {
+  const addDiv = document.querySelector(".imagecontainer");
+  const div = document.createElement("div");
+  addDiv.appendChild(div);
+  div.classList.add("imageWrapper");
+  const newImage = document.createElement("img");
+  newImage.src =
+    "https://via.assets.so/game.png?id=1&q=95&w=360&h=360&fit=fill";
+  newImage.alt = "photo 2";
+  div.appendChild(newImage);
+  disabledBtn();
+}
+
+function removeImg() {
+  let removeImgEl = document.querySelector(".imagecontainer");
+  removeImgEl.removeChild(removeImgEl.lastChild);
+  disabledBtn();
+}
+
+function disabledBtn() {
+  let imageElement = document.querySelectorAll("img");
+  if (imageElement.length < 2) {
+    document.querySelector(".removeBtn").disabled = true;
+  } else {
+    document.querySelector(".removeBtn").disabled = false;
+  }
 }
 
 window.addEventListener("DOMContentLoaded", init);
