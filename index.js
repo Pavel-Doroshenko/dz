6a35f764c1c2411b8030ffe2491a322795f4c0bd
pavel <grelkin07@mail.ru>
2025-03-21 01:25:58 +0000

    финальные  правки предпросмотра и рефакторинг кода

---
+++ index.js
@@ -0,0 +1,58 @@
+function init() {
+  document.querySelector(".newImg").onclick = refresh;
+  document.querySelector(".addBtn").onclick = addImg;
+  document.querySelector(".removeBtn").onclick = removeImg;
+  document.querySelector(".container").addEventListener("click", zoomIn);
+  document.querySelectorAll("img").forEach((image) => {
+    image.onload = (Event) => Event.target.classList.remove("loading");
+  });
+  console.log("init");
+}
+
+function refresh() {
+  document.querySelectorAll("img").forEach((image) => {
+    image.src = "https://loremflickr.com/320/240?random=" + Math.random();
+    image.classList.add("loading");
+  });
+  console.log("refresh");
+}
+
+function zoomIn(event) {
+  if (event.target.tagName === "IMG") {
+    const parent = event.target.parentElement;
+    parent.classList.toggle("fullScreen");
+  }
+  if (event.target.classList.contains("fullScreen")) {
+    event.target.classList.toggle("fullScreen");
+  }
+  console.log("zoomIn");
+}
+
+function addImg() {
+  const addDiv = document.querySelector(".imagecontainer");
+  const div = document.createElement("div");
+  addDiv.appendChild(div);
+  div.classList.add("imageWrapper");
+  const newImage = document.createElement("img");
+  newImage.src = "https://loremflickr.com/320/240?random=" + Math.random();
+  newImage.alt = "photo 2";
+  div.appendChild(newImage);
+  disabledBtn();
+}
+
+function removeImg() {
+  let removeImgEl = document.querySelector(".imagecontainer");
+  removeImgEl.removeChild(removeImgEl.lastChild);
+  disabledBtn();
+}
+
+function disabledBtn() {
+  let imageElement = document.querySelectorAll("img");
+  if (imageElement.length < 2) {
+    document.querySelector(".removeBtn").disabled = true;
+  } else {
+    document.querySelector(".removeBtn").disabled = false;
+  }
+}
+
+window.addEventListener("DOMContentLoaded", init);
